562ad372dc837fe34514f2a53a4ecc32e76110de
Gedeon Kakona <gedeonkakona@gmail.com>
2022-12-30 10:35:27 +0200

    connecting client and server

--- client/script.js
+++ client/script.js
@@ -60,7 +60,33 @@ const handleSubmit = async (e) => {
   chatContainer.innerHTML += chatStripe(true, '', uniqueId);
   chatContainer.scrollTop = chatContainer.scrollHeight;
   const messageDiv = document.getElementById(uniqueId);
+  // loading messageDiv.innerHTML = "..."
   loader(messageDiv);
+  // fetch data from the server
+  const response = await fetch('http://localhost:5000', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      prompt: data.get('prompt'),
+    }),
+  });
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = ' ';
+
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim(); // trims any trailing spaces/'\n'
+
+    typeText(messageDiv, parsedData);
+  } else {
+    const err = await response.text();
+
+    messageDiv.innerHTML = 'Something went wrong';
+    alert(err);
+  }
 };
 
 form.addEventListener('submit', handleSubmit);
